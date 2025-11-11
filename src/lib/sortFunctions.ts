c61a4f5cc5e1d39172c7797a9af6edafe2a67589
Nikita Chursin <chursinn91@gmail.com>
2025-11-11 21:43:55 +0300

    fixed sorting

--- src/lib/sortFunctions.ts
+++ src/lib/sortFunctions.ts
@@ -18,6 +18,30 @@ export const sortByTitle = (entries: GenericEntry[]): GenericEntry[] => {
   return sortedEntries;
 };
 
+// Sort by title
+export const sortByPubDate = (entries: GenericEntry[]): GenericEntry[] => {
+  console.log(`--------------------`);
+  console.log(`entries >>  ${entries.map((x) => x.data.title)}`);
+
+  const sortedEntries = entries.sort((a: any, b: any) => {
+    console.log(`a.data.title >> ${a.data.title}`);
+    console.log(`a.data.pubDate >> ${a.data.pubDate}`);
+
+    console.log(`b.data.title >> ${b.data.title}`);
+    console.log(`b.data.pubDate >> ${b.data.pubDate}`);
+    const result =
+      new Date(b.data.pubDate && b.data.pubDate).valueOf() -
+      new Date(a.data.pubDate && a.data.pubDate).valueOf();
+
+    console.log(`result >> ${result}`);
+    return result;
+  });
+
+  console.log(`entries >>  ${entries.map((x) => x.data.title)}`);
+  console.log(`sortedEntries >>  ${sortedEntries.map((x) => x.data.title)}`);
+  return sortedEntries;
+};
+
 // Sort by random
 export const sortByRandom = (entries: GenericEntry[]): GenericEntry[] => {
   const sortedEntries = entries.sort(() => Math.random() - 0.5);
